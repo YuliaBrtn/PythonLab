137c82a51e1f65b49434b72ce7b7cd0a484c4caa
Yulia <giuliabrtn07@gmail.com>
2025-11-16 01:30:23 +0300

    code is done

--- Lab 2 Task 1/main.py
+++ Lab 2 Task 1/main.py
@@ -1,10 +1,9 @@
 def multiply_digits(n):
     product = 1
-    # Используем цикл for для итерации по строке
     for digit in str(n):
         if digit.isdigit():
             product *= int(digit)
     return product
 
-N = 1234
+N = 5981
 print(f"Произведение цифр {N}: {multiply_digits(N)}")
