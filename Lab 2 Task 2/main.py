c8b5a995239b72852c440ba04b60c56c0b531b9e
Yulia <giuliabrtn07@gmail.com>
2025-11-16 01:46:38 +0300

    code is done

---
+++ Lab 2 Task 2/main.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+result = []
+for num in numbers:
+    if num % 2 != 0:
+        result.append(num)
+print(f"Список без чётных чисел: {result}")
